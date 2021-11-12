b0321ee9a052f6515b912b7758c3f98927770f8d
Ivan Laver <ereyzer142@gmail.com>
2021-11-12 12:14:02 +0200

    add DB connect

--- repository/db-start.js
+++ repository/db-start.js
@@ -1,1 +1,25 @@
-module.exports = {}
+const mongoose = require("mongoose")
+
+const { URI_DB } = require("../config/dotenv-config")
+
+const db = mongoose.connect(URI_DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+
+mongoose.connection.on("connected", () => {
+	console.log("Database connection successful")
+})
+
+mongoose.connection.on("error", err => {
+	console.log(`Mongoose connection error ${err.message}`)
+	process.exit(1)
+})
+
+process.on("SIGINT", async () => {
+	await mongoose.connection.close()
+	console.log("Connection to DB closed")
+	process.exit()
+})
+
+module.exports = db
