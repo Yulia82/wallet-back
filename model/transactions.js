f40bef75d048b6ceda5f24c8c2471539e42975e1
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-11-12 20:56:12 +0200

    create transactuin schema

--- model/transactions.js
+++ model/transactions.js
@@ -1,1 +1,25 @@
-module.exports = {};
+const { Schema, model } = require("mongoose")
+
+const transactionSchema = new Schema(
+	{
+		type: { type: String, required: true },
+		category: { type: String, required: true },
+		sum: { type: Number, required: true },
+	},
+	{
+		versionKey: false,
+		timestamps: true, //! Добавил время для созданной транзакции
+		toJSON: {
+			virtuals: true,
+			transform: function (doc, ret) {
+				delete ret._id //! удалил поле _id для front
+				return ret
+			},
+		},
+		toObject: { virtuals: true },
+	},
+)
+
+const Transaction = model("transactions", transactionSchema)
+
+module.exports = { Transaction }
