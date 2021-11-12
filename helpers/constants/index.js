53b23a64d89b090b60887ecf16954ad099ecac4f
Ivan Laver <ereyzer142@gmail.com>
2021-11-12 18:56:15 +0200

    add errorHendler and some errors

--- helpers/constants/index.js
+++ helpers/constants/index.js
@@ -1,4 +1,5 @@
 const HttpCode = require("./httpConstants")
 const ValidNameLength = require("./validateConstants")
+const errorConstants = require("./error-constants")
 
-module.exports = { HttpCode, ValidNameLength }
+module.exports = { HttpCode, ValidNameLength, errorConstants }
---
+++ helpers/errorHandler/index.js
@@ -0,0 +1,4 @@
+const CustomError = require("./customError")
+const wrapperError = require("./errorHandler")
+
+module.exports = { CustomError, wrapperError }
