6f1c73215cd88389f9c3763279b356d4b0ba07ca
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-11-13 12:41:45 +0200

    Update guard.js

--- helpers/guard/guard.js
+++ helpers/guard/guard.js
@@ -1,19 +1,7 @@
 const passport = require("passport")
 require("../config/passport")
 const { HttpCode } = require("../constants/httpConstants")
-const {
-	OK,
-	CREATED,
-	ACCEPTED,
-	NO_CONTENT,
-	BAD_REQUEST,
-	UNAUTHORIZED,
-	FORBIDDEN,
-	NOT_FOUND,
-	CONFLICT,
-	TOO_MANY_REQUESTS,
-	INTERNAL_SERVER_ERROR,
-} = HttpCode
+const { UNAUTHORIZED } = HttpCode
 
 const guard = (req, res, next) => {
 	passport.authenticate("jwt", { session: false }, (error, user) => {
