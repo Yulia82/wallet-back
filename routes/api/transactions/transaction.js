f91fa21d305fa4caa483fdff19e2bfacea866743
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-11-15 12:46:36 +0200

    done delete transaction by id

--- routes/api/transactions/transaction.js
+++ routes/api/transactions/transaction.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const { transactionControllers } = require("../../../controllers")
-const { validateTransaction } = require("../../validation")
+const { validateTransaction, validateId } = require("../../validation")
 const { guard } = require("../../../helpers/guard")
 
-router.post("/create", validateTransaction, transactionControllers.saveTransaction)
+router.post("/", validateTransaction, transactionControllers.saveTransaction)
+router.delete("/:id", validateId, transactionControllers.deleteTransaction)
 
 module.exports = router
