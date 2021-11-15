8b5cc8efa9a59d825b410278b6ab10f13ba9f5fd
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-11-15 14:16:53 +0200

    fixed all bag

--- services/email/service.js
+++ services/email/service.js
@@ -8,8 +8,8 @@ class EmailService {
 		switch (env.NODE_ENV) {
 			case "development":
 				// this.link = " https://5285-91-221-219-76.ngrok.io";
-				// this.link = `http://${env.HOST}:${env.PORT}`
-				this.link = "http://27c7-185-19-6-75.ngrok.io"
+				this.link = `http://${env.HOST}:${env.PORT}`
+				// this.link = "http://27c7-185-19-6-75.ngrok.io"
 
 				break
 			case "production":
@@ -17,8 +17,8 @@ class EmailService {
 
 				break
 			default:
-				// this.link = `http://${env.HOST}:${env.PORT}`
-				this.link = "http://27c7-185-19-6-75.ngrok.io"
+				this.link = `http://${env.HOST}:${env.PORT}`
+				// this.link = "http://27c7-185-19-6-75.ngrok.io"
 
 				break
 		}
