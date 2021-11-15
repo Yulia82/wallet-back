f6c56fb6c76ef370c0dd802d31c9a7b18e447d19
Ivan Laver <ereyzer142@gmail.com>
2021-11-15 15:18:50 +0200

    test fix before deploy

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ const { db } = require("../repository")
 const { PORT, HOST } = require("../config/dotenv-config")
 
 db.then(() => {
-	app.listen(PORT, HOST, async () => {
+	app.listen(PORT, async () => {
 		console.log(`Server running. Use our API on http://${HOST}:${PORT}`)
 	})
 }).catch(e => {
