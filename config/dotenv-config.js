f4ed52f7d43dc2d7d4b036a4dc8cab0e4b4a3139
Ivan Laver <ereyzer142@gmail.com>
2021-11-15 17:34:10 +0200

    deploy

--- config/dotenv-config.js
+++ config/dotenv-config.js
@@ -1,3 +1,3 @@
 require("dotenv").config()
-console.log("process.env.HOSTprocess.env.HOSTprocess.env.HOST", process.env)
+console.log("process.env.HOSTprocess.env.HOSTprocess.env.HOST", process.env.port)
 module.exports = { PORT: 4040, ...process.env }
